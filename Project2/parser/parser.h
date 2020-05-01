0694bb0acbe43c32078658ec77b234e854d207b9
Miguel Pinto <43591348+MiguelDelPinto@users.noreply.github.com>
2020-05-01 21:57:50 +0100

    Added deleted files

---
+++ Project2/parser/parser.h
@@ -0,0 +1,23 @@
+
+#include "../client/ClientArgs.h"
+#include "../server/ServerArgs.h"
+
+/**
+ * @brief Parses the command line arguments of the client process (U)
+ * 
+ * @param argc number of arguments
+ * @param argv array of argument string
+ * @param clientArgs struct that holds the parsed results
+ * @return int 0 if successful, anything else if otherwise
+ */
+int parse_client_args(int argc, char *argv[], struct ClientArgs * clientArgs);
+
+/**
+ * @brief Parses the command line arguments of the server process (Q)
+ * 
+ * @param argc number of arguments
+ * @param argv array of argument string
+ * @param serverArgs struct that holds the parsed results
+ * @return int 0 if successful, anything else if otherwises
+ */
+int parse_server_args(int argc, char *argv[],struct ServerArgs * serverArgs);
